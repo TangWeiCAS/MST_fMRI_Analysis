50586d8349b8f8d1ce0ad1d954da6e85893075dc
Tang Wei <105002091+TangWeiCAS@users.noreply.github.com>
2023-09-20 18:47:51 +0800

    all 1st-level in SPM12
    default, without B0 correction
    realign-slicetiming-coreg-segment-normalise-smooth-run1st-estimate1st

---
+++ est1stlevel.m
@@ -0,0 +1,8 @@
+%-----------------------------------------------------------------------
+% Job saved on 20-Sep-2023 16:58:45 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_est.spmmat = {'E:\SCDpattern\2023014\9004\1stLevel\SPM.mat'};
+matlabbatch{1}.spm.stats.fmri_est.write_residuals = 1;
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
